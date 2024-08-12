a4d23230b58fbd47e1a0752e95782064e1832ec6
CharlesZambrano <cdzambrano3@espe.edu.ec>
2024-08-12 11:35:37 -0500

    Reconigtion deepface

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,10 @@
+import json
+
 import cv2
 import numpy as np
-from ultralytics import \
-    YOLO  # Importar la clase YOLO desde la librería ultralytics
+from ultralytics import YOLO
 
+from deepface import DeepFace
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -34,6 +36,37 @@ def detect_faces():
         })
     
     return jsonify({"faces": faces})
+
+@app.route('/recognize', methods=['POST'])
+def recognize_faces():
+    # Recibir la imagen
+    file = request.files['image'].read()
+    np_img = np.frombuffer(file, np.uint8)
+    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+    
+    # Recibir las coordenadas del rostro desde form-data como texto y convertirlas a JSON
+    faces = json.loads(request.form['faces'])
+    
+    identities = []
+    
+    for face in faces:
+        x1, y1, x2, y2 = face['x1'], face['y1'], face['x2'], face['y2']
+        
+        # Recortar la región de la imagen donde está el rostro
+        face_img = img[y1:y2, x1:x2]
+        
+        # Realizar el reconocimiento facial
+        try:
+            results = DeepFace.find(face_img, db_path='/app/employes_database', enforce_detection=False)
+            if results:  # Verifica que la lista de resultados no esté vacía
+                identity = results[0]['identity']  # Esto es probablemente una serie de Pandas
+                identities.append(str(identity))  # Convertir la identidad a cadena de texto
+            else:
+                identities.append("Unknown")
+        except Exception as e:
+            identities.append("Error: " + str(e))
+    
+    return jsonify({"identities": identities})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
