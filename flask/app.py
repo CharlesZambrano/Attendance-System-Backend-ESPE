d757d73f4b96771391314d7680f6ea7066b90b18
CharlesZambrano <cdzambrano3@espe.edu.ec>
2024-08-12 10:45:23 -0500

    Deteccion de rostro con yolo

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,14 @@
-# app.py
 import cv2
 import numpy as np
-import torch
+from ultralytics import \
+    YOLO  # Importar la clase YOLO desde la librería ultralytics
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Cargar el modelo YOLO desde el volumen compartido
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='/app/yolo_workspace/yolov8l-face-lindevs.pt')
+# Cargar el modelo YOLOv8 desde el directorio correcto
+model = YOLO('/app/yolov8l-face-lindevs.pt')
 
 @app.route('/detect', methods=['POST'])
 def detect_faces():
@@ -22,14 +22,14 @@ def detect_faces():
     
     # Extraer coordenadas de los rostros detectados
     faces = []
-    for result in results.xyxy[0]:
-        x1, y1, x2, y2, conf, cls = map(float, result)
+    for result in results[0].boxes.data.cpu().numpy():
+        x1, y1, x2, y2, conf, cls = result
         faces.append({
             'x1': int(x1),
             'y1': int(y1),
             'x2': int(x2),
             'y2': int(y2),
-            'confidence': conf,
+            'confidence': float(conf),
             'class': int(cls)
         })
     
