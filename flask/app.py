f896b642b0aadef84a784afcd466ed94616acbd9
CharlesZambrano <cdzambrano3@espe.edu.ec>
2024-08-12 12:04:06 -0500

    Reconocimiento

--- flask/app.py
+++ flask/app.py
@@ -47,7 +47,7 @@ def recognize_faces():
     # Recibir las coordenadas del rostro desde form-data como texto y convertirlas a JSON
     faces = json.loads(request.form['faces'])
     
-    identities = []
+    match_counts = {}  # Diccionario para contar coincidencias por identidad
     
     for face in faces:
         x1, y1, x2, y2 = face['x1'], face['y1'], face['x2'], face['y2']
@@ -58,15 +58,25 @@ def recognize_faces():
         # Realizar el reconocimiento facial
         try:
             results = DeepFace.find(face_img, db_path='/app/employes_database', enforce_detection=False)
-            if results:  # Verifica que la lista de resultados no esté vacía
-                identity = results[0]['identity']  # Esto es probablemente una serie de Pandas
-                identities.append(str(identity))  # Convertir la identidad a cadena de texto
-            else:
-                identities.append("Unknown")
+            if results:  # Verifica que la lista no esté vacía
+                for df in results:
+                    if not df.empty:
+                        for index, row in df.iterrows():
+                            identity = row['identity']  # Extraer el valor de la serie
+                            identity_name = identity.split('/')[-2]  # Extraer el nombre del subdirectorio (identidad)
+                            if identity_name in match_counts:
+                                match_counts[identity_name] += 1
+                            else:
+                                match_counts[identity_name] = 1
         except Exception as e:
-            identities.append("Error: " + str(e))
+            return jsonify({"identities": ["Error: " + str(e)]})
     
-    return jsonify({"identities": identities})
+    # Verificar si hay alguna identidad con 10 o más coincidencias
+    for identity_name, count in match_counts.items():
+        if count >= 10:
+            return jsonify({"identities": [identity_name]})
+    
+    return jsonify({"identities": ["Desconocido"]})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
